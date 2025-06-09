f88cfeb6abd53b0ea9431cc9ad663710c8f46e56
Fenix-LAB <fenixcr.123@gmail.com>
2025-06-08 21:04:09 -0600

    Add support for Spanish language in language schema

--- src/hooks/language/schema.ts
+++ src/hooks/language/schema.ts
@@ -3,11 +3,13 @@ import { z } from 'zod';
 export const enum SupportedLanguages {
   EN_EN = 'en-EN',
   FR_FR = 'fr-FR',
+  ES_ES = 'es-ES',
 }
 
 export const languageSchema = z.enum([
   SupportedLanguages.EN_EN,
   SupportedLanguages.FR_FR,
+  SupportedLanguages.ES_ES,
 ]);
 
 export type Language = z.infer<typeof languageSchema>;
